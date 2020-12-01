dfdc65b60d773988df8236b6408e8f5d6c31e0c2
Sunnybird88 <69608484+Sunnybird88@users.noreply.github.com>
2020-11-30 19:05:15 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Class34
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ ball.js
@@ -0,0 +1,21 @@
+class Ball {
+    constructor(x, y, radius) {
+        var options = {
+            density: 1,
+            friction: 0.005
+        }
+        this.body = Bodies.circle(x, y, radius, options);
+        World.add(world, this.body);
+        this.radius = radius;
+    }
+
+    display() {
+        var angle = this.body.angle
+        push()
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle)
+        ellipseMode(RADIUS)
+        ellipse(0, 0, this.radius, this.radius)
+        pop()
+    }
+}
